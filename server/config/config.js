8604b98cb159f288c90d28903a059009679a1fdd
Guy Levesque <guy.leo.levesque@gmail.com>
2018-12-28 19:27:00 -0400

    Added File

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+let EZ_ENV = {
+  secret_key: "5bd9e660f94a1957901c731d",
+  appYear: 2018,
+  BUCKET_NAME: 'ez-hst-canada-img-upload',
+  IAM_USER_KEY: 'AKIAIQEEXX33FFDATFHA',
+  IAM_USER_SECRET: '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'
+};
+
+module.exports = { EZ_ENV };
